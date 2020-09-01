6e56591ba1261ab6e0add11d62e31e092ae86a70
Vinit Kallampad <vinit.kallampad@gmail.com>
2020-08-31 17:31:58 -0700

    Replacing Ones

---
+++ src/main/java/com/mkv/codingpatterns/slidingwindow/ReplacingOnes.java
@@ -0,0 +1,18 @@
+package com.mkv.codingpatterns.slidingwindow;
+
+/*
+Given an array containing 0s and 1s, if you are allowed to replace no more than ‘k’ 0s with 1s,
+find the length of the longest contiguous subarray having all 1s.
+*/
+
+public class ReplacingOnes {
+
+
+
+
+	public static void main(String[] args) {
+		ReplacingOnes main = new ReplacingOnes();
+
+
+	}
+}
