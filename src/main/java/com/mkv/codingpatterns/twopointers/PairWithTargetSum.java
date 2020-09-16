cf51c3d86e135b2531796103adee2b10cd88a39f
Vinit Kallampad <vinit.kallampad@gmail.com>
2020-09-15 19:08:26 -0700

    Pair with Target sum using HashMap

--- src/main/java/com/mkv/codingpatterns/twopointers/PairWithTargetSum.java
+++ src/main/java/com/mkv/codingpatterns/twopointers/PairWithTargetSum.java
@@ -4,6 +4,9 @@ package com.mkv.codingpatterns.twopointers;
 find a pair in the array whose sum is equal to the given target.
 */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PairWithTargetSum {
 
 	private int[] searchPair(int[] array, int sum) {
@@ -28,5 +31,20 @@ public class PairWithTargetSum {
 		result = main.searchPair(new int[] { 2, 5, 9, 11 }, 11);
 		System.out.println("Pair with target sum: [" + result[0] + ", " + result[1] + "]");
 
+		result = main.searchPairUsingHashMap(new int[] { 1, 2, 3, 4, 6 }, 6);
+		System.out.println("Pair with target sum: [" + result[0] + ", " + result[1] + "]");
+		result = main.searchPairUsingHashMap(new int[] { 2, 5, 9, 11 }, 11);
+		System.out.println("Pair with target sum: [" + result[0] + ", " + result[1] + "]");
+
+	}
+
+	private int[] searchPairUsingHashMap(int[] array, int sum) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for(int i = 0; i < array.length; i++) {
+			if(map.containsKey(sum - array[i]))
+				return new int[] {map.get(sum - array[i]), i};
+			map.put(array[i], i);
+		}
+		return new int[] {-1, -1};
 	}
 }
