929071166847a4a6306446a25dec02f8ac58d722
Vinit Kallampad <vinit.kallampad@gmail.com>
2020-09-24 19:08:48 -0700

    Subarrays with Product Less than a Target - Implementation

--- src/main/java/com/mkv/codingpatterns/twopointers/SubArrayProductLessThanK.java
+++ src/main/java/com/mkv/codingpatterns/twopointers/SubArrayProductLessThanK.java
@@ -3,31 +3,41 @@ package com.mkv.codingpatterns.twopointers;
 /*
 Given an array with positive numbers and a target number, find all of its contiguous subarrays
 whose product is less than the target number.
+
+Time complexity #
+The main for-loop managing the sliding window takes O(N) but creating subarrays can take up to O(N^2) in the worst case.
+Therefore overall, our algorithm will take O(N^3).
+
+Space complexity #
+Ignoring the space required for the output list, the algorithm runs in O(N) space which is used for the temp list.
+
 */
 
 import java.util.ArrayList;
 import java.util.List;
 
-/*
-Given an array with positive numbers and a target number, find all of its contiguous subarrays
-whose product is less than the target number.
-*/
-
 public class SubArrayProductLessThanK {
 
 	private List<List<Integer>> findSubArray(int[] array, int target) {
 		List<List<Integer>> list = new ArrayList<>();
-		int product = 1, start = 0;
+		int product = 1, start = 0, count = 0;
 
 		List<Integer> subList = new ArrayList<>();
 		for(int end = 0; end < array.length; end++) {
 			product *= array[end];
 			while(product >= target && start < array.length) {
+				product /= array[start++];
 
-
+			}
+			count += end - start + 1;
+			List<Integer> innerList = new ArrayList<>();
+			for(int i = start; i <= end; i++){
+				innerList.add(array[i]);
+				list.add(innerList);
 			}
 
 		}
+		System.out.println(count);
 		return list;
 	}
 
