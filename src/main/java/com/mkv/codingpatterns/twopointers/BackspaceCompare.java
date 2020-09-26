214f3665474a4eda27a129dd9deaed780500439d
Vinit Kallampad <vinit.kallampad@gmail.com>
2020-09-25 20:22:40 -0700

    Compare String with Backspace using Two Pointer Method

--- src/main/java/com/mkv/codingpatterns/twopointers/BackspaceCompare.java
+++ src/main/java/com/mkv/codingpatterns/twopointers/BackspaceCompare.java
@@ -1,7 +1,15 @@
 package com.mkv.codingpatterns.twopointers;
 
 /*Given two strings containing backspaces
-(identified by the character ‘#’), check if the two strings are equal.*/
+(identified by the character ‘#’), check if the two strings are equal.
+
+Time complexity #
+The time complexity of the above algorithm will be O(M+N) where ‘M’ and ‘N’ are the lengths of the two input strings respectively.
+
+Space complexity #
+The algorithm runs in constant space O(1).
+
+*/
 
 public class BackspaceCompare {
 
@@ -25,16 +33,61 @@ public class BackspaceCompare {
 			}
 
 		}
-		System.out.println(tempStr.toString());
 		return tempStr.toString();
+	}
+
+	private boolean compareStringTwoPointer(String str1, String str2) {
+		int i1, i2;
+		int index1 = str1.length() - 1, index2 = str2.length() - 1;
+
+		while(index1 >= 0|| index2 >= 0) {
+			System.out.println();
+			i1 = getNextValidCharacter(str1, index1);
+			System.out.println();
+			i2 = getNextValidCharacter(str2, index2);
+
+			if(i1 < 0 && i2 < 0)
+				return true;
+			if(i1 < 0 || i2 < 0)
+				return false;
+			if(str1.charAt(i1) != str2.charAt(i2))
+				return false;
+			index1 = i1-1;
+			index2 = i2-1;
+		}
+		return true;
+	}
+
+	private int getNextValidCharacter(String str, int index) {
+		int backspace = 0;
+		while(index > 0) {
+			if(str.charAt(index) == '#')
+				backspace++;
+			else {
+				if(backspace > 0)
+					backspace--;
+				else
+					break;
+			}
+			index--;
+		}
+		System.out.print(str +"  "+index + " ");
+		return index;
 	}
 
 	public static void main(String[] args) {
 		BackspaceCompare main = new BackspaceCompare();
 
-		System.out.println(main.compareString("xy#z", "xzz#"));
+/*		System.out.println(main.compareString("xy#z", "xzz#"));
 		System.out.println(main.compareString("xy#z", "xyz#"));
 		System.out.println(main.compareString("xp#", "xyz##"));
 		System.out.println(main.compareString("xywrrmp", "xywrrmu#p"));
+*/
+
+		System.out.println(main.compareStringTwoPointer("xy#z", "xzz#"));
+		System.out.println(main.compareStringTwoPointer("xy#z", "xyz#"));
+		System.out.println(main.compareStringTwoPointer("xp#", "xyz##"));
+		System.out.println(main.compareStringTwoPointer("xywrrmp", "xywrrmu#p"));
+
 	}
 }
