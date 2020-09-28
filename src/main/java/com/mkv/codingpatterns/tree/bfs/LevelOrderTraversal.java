518a9fbf8aa3e0c8c312ae60859ade036cd5ecf6
Vinit Kallampad <vinit.kallampad@gmail.com>
2020-09-28 16:02:45 -0700

    Level Order Traversal for Tree - Adding Time and space complexity

--- src/main/java/com/mkv/codingpatterns/tree/bfs/LevelOrderTraversal.java
+++ src/main/java/com/mkv/codingpatterns/tree/bfs/LevelOrderTraversal.java
@@ -3,6 +3,16 @@ package com.mkv.codingpatterns.tree.bfs;
 /*
 Given a binary tree, populate an array to represent its level-by-level traversal.
 You should populate the values of all nodes of each level from left to right in separate sub-arrays.
+
+Time complexity #
+The time complexity of the above algorithm is O(N), where ‘N’ is the total number of nodes in the tree.
+This is due to the fact that we traverse each node once.
+
+Space complexity #
+The space complexity of the above algorithm will be O(N) as we need to return a list containing the level order traversal.
+We will also need O(N) space for the queue. Since we can have a maximum of N/2 nodes at any level (this could happen only at the lowest level),
+therefore we will need O(N)O(N) space to store them in the queue.
+
 */
 
 import java.util.ArrayDeque;
