e4da700be9776a5154ac42e7905cb9a390f2aaf2
Vinit Kallampad <vinit.kallampad@gmail.com>
2020-10-20 18:57:09 -0700

    Fast Slow Pointer - Finding LinkedList Cycle

---
+++ src/main/java/com/mkv/codingpatterns/fastandslowpointers/LinkedListCycle.java
@@ -0,0 +1,36 @@
+package com.mkv.codingpatterns.fastandslowpointers;
+
+public class LinkedListCycle {
+
+	private boolean hasCycle(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head;
+
+		while(fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if(fast == slow)
+				return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+
+		LinkedListCycle main = new LinkedListCycle();
+
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = new ListNode(4);
+		head.next.next.next.next = new ListNode(5);
+		head.next.next.next.next.next = new ListNode(6);
+		System.out.println("LinkedList has cycle: " + main.hasCycle(head));
+
+		head.next.next.next.next.next.next = head.next.next;
+		System.out.println("LinkedList has cycle: " + main.hasCycle(head));
+
+		head.next.next.next.next.next.next = head.next.next.next;
+		System.out.println("LinkedList has cycle: " + main.hasCycle(head));
+	}
+}
---
+++ src/main/java/com/mkv/codingpatterns/fastandslowpointers/ListNode.java
@@ -0,0 +1,10 @@
+package com.mkv.codingpatterns.fastandslowpointers;
+
+class ListNode {
+	private int value;
+	ListNode next;
+
+	ListNode(int value) {
+		this.value = value;
+	}
+}
