8d24272aee666e65cf25b8ec0713a14ceeacfeeb
Vinit Kallampad <vinit.kallampad@gmail.com>
2020-11-02 19:36:05 -0800

    Reverse Sub List of size K - reformatting code

--- src/main/java/com/mkv/codingpatterns/inplacereversallinkedlist/ReverseEveryKElements.java
+++ src/main/java/com/mkv/codingpatterns/inplacereversallinkedlist/ReverseEveryKElements.java
@@ -13,53 +13,49 @@ We only used constant space, therefore, the space complexity of our algorithm is
 
 public class ReverseEveryKElements {
 
-	private Node reverse(Node head, int k) {
-		Node next, current = head, previous = null;
-		Node lastPartOfSubList, lastNodeOfPreviousPart ;
-		while(true) {
-			lastPartOfSubList = current;
-			lastNodeOfPreviousPart = previous;
-			for(int i = 0; current != null && i < k; i++) {
-				next = current.next;
-				current.next = previous;
-				previous = current;
-				current = next;
-			}
+  private Node reverse(Node head, int k) {
+    Node next, current = head, previous = null;
+    Node lastPartOfSubList, lastNodeOfPreviousPart;
+    while (true) {
+      lastPartOfSubList = current;
+      lastNodeOfPreviousPart = previous;
+      for (int i = 0; current != null && i < k; i++) {
+        next = current.next;
+        current.next = previous;
+        previous = current;
+        current = next;
+      }
 
-			if(lastNodeOfPreviousPart != null)
-				lastNodeOfPreviousPart.next = previous;
-			else
-				head = previous;
+      if (lastNodeOfPreviousPart != null) lastNodeOfPreviousPart.next = previous;
+      else head = previous;
 
-			lastPartOfSubList.next = current;
+      lastPartOfSubList.next = current;
 
-			if(current == null)
-				break;
+      if (current == null) break;
 
-			previous = lastPartOfSubList;
-		}
-		return head;
-	}
+      previous = lastPartOfSubList;
+    }
+    return head;
+  }
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		ReverseEveryKElements main = new ReverseEveryKElements();
+    ReverseEveryKElements main = new ReverseEveryKElements();
 
-		Node head = new Node(1);
-		head.next = new Node(2);
-		head.next.next = new Node(3);
-		head.next.next.next = new Node(4);
-		head.next.next.next.next = new Node(5);
-		head.next.next.next.next.next = new Node(6);
-		head.next.next.next.next.next.next = new Node(7);
-		head.next.next.next.next.next.next.next = new Node(8);
+    Node head = new Node(1);
+    head.next = new Node(2);
+    head.next.next = new Node(3);
+    head.next.next.next = new Node(4);
+    head.next.next.next.next = new Node(5);
+    head.next.next.next.next.next = new Node(6);
+    head.next.next.next.next.next.next = new Node(7);
+    head.next.next.next.next.next.next.next = new Node(8);
 
-		Node result = main.reverse(head, 3);
-		System.out.print("Nodes of the reversed LinkedList are: ");
-		while (result != null) {
-			System.out.print(result.value + " ");
-			result = result.next;
-		}
-	}
-
+    Node result = main.reverse(head, 3);
+    System.out.print("Nodes of the reversed LinkedList are: ");
+    while (result != null) {
+      System.out.print(result.value + " ");
+      result = result.next;
+    }
+  }
 }
