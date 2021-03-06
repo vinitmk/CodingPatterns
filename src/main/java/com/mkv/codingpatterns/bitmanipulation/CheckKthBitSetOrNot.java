9dac9365a75c9d528d476faf8b22d2faa090b6cb
vinitmk <vinit.kallampad@gmail.com>
2021-03-06 12:14:52 -0800

    Bit Manipulation: Left Shift

--- src/main/java/com/mkv/codingpatterns/bitmanipulation/CheckKthBitSetOrNot.java
+++ src/main/java/com/mkv/codingpatterns/bitmanipulation/CheckKthBitSetOrNot.java
@@ -2,16 +2,16 @@ package com.mkv.codingpatterns.bitmanipulation;
 
 public class CheckKthBitSetOrNot {
 
-	private boolean getKthBit(int n, int k) {
+	private boolean getKthBitUsingLeftShift(int n, int k) {
 		return (n & (1 << (k-1))) != 0;
 	}
 
 	public static void main(String[] args) {
 		CheckKthBitSetOrNot main = new CheckKthBitSetOrNot();
 
-		boolean k = main.getKthBit(5, 3);
+		boolean k = main.getKthBitUsingLeftShift(5, 3);
 		System.out.println(k);
-		System.out.println("n = 10, k = 2 : " + main.getKthBit(10, 2));
+		System.out.println("n = 10, k = 2 : " + main.getKthBitUsingLeftShift(10, 2));
 	}
 
 }
--- src/main/java/com/mkv/codingpatterns/bitmanipulation/FirstSetBitPosition.java
+++ src/main/java/com/mkv/codingpatterns/bitmanipulation/FirstSetBitPosition.java
@@ -2,7 +2,7 @@ package com.mkv.codingpatterns.bitmanipulation;
 
 public class FirstSetBitPosition {
 
-	private int getFirstSetBit(int n) {
+	private int getFirstSetBitUsingLeftShift(int n) {
 		int k = 1;
 		while(true) {
 			if((n & (1 << k-1)) == 0)
@@ -16,8 +16,8 @@ public class FirstSetBitPosition {
 
 		FirstSetBitPosition main = new FirstSetBitPosition();
 
-		System.out.println("First setbit position for number: 18 is -> " + main.getFirstSetBit(18));
-		System.out.println("First setbit position for number: 5 is -> " + main.getFirstSetBit(5));
-		System.out.println("First setbit position for number: 32 is -> " + main.getFirstSetBit(32));
+		System.out.println("First setbit position for number: 18 is -> " + main.getFirstSetBitUsingLeftShift(18));
+		System.out.println("First setbit position for number: 5 is -> " + main.getFirstSetBitUsingLeftShift(5));
+		System.out.println("First setbit position for number: 32 is -> " + main.getFirstSetBitUsingLeftShift(32));
 	}
 }
