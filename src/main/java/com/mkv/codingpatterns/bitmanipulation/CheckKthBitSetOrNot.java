5d0f05060f9e84d99ebc19ecf9624b20ae5ddaa4
vinitmk <vinit.kallampad@gmail.com>
2021-03-06 12:25:28 -0800

    Bit Manipulation: Right Shift

--- src/main/java/com/mkv/codingpatterns/bitmanipulation/CheckKthBitSetOrNot.java
+++ src/main/java/com/mkv/codingpatterns/bitmanipulation/CheckKthBitSetOrNot.java
@@ -6,12 +6,19 @@ public class CheckKthBitSetOrNot {
 		return (n & (1 << (k-1))) != 0;
 	}
 
+	private boolean getKthBitUsingRightShift(int n, int k) {
+		return ((n >> (k-1)) & 1) == 1;
+	}
+
 	public static void main(String[] args) {
 		CheckKthBitSetOrNot main = new CheckKthBitSetOrNot();
 
 		boolean k = main.getKthBitUsingLeftShift(5, 3);
 		System.out.println(k);
 		System.out.println("n = 10, k = 2 : " + main.getKthBitUsingLeftShift(10, 2));
+
+		System.out.println("n = 10, k = 2 : " + main.getKthBitUsingRightShift(10, 2));
+		System.out.println("n = 10, k = 1 : " + main.getKthBitUsingRightShift(10, 1));
 	}
 
 }
--- src/main/java/com/mkv/codingpatterns/bitmanipulation/FirstSetBitPosition.java
+++ src/main/java/com/mkv/codingpatterns/bitmanipulation/FirstSetBitPosition.java
@@ -12,6 +12,22 @@ public class FirstSetBitPosition {
 		}
 	}
 
+	private int getFirstSetBitUsingRightShift(int n) {
+		int k = 1;
+		if(n == 0)
+			return 0;
+
+		while(true) {
+
+			if(((n >> k-1) & 1) == 1)
+				return k;
+			else
+				k++;
+
+		}
+
+	}
+
 	public static void main(String[] args) {
 
 		FirstSetBitPosition main = new FirstSetBitPosition();
@@ -19,5 +35,9 @@ public class FirstSetBitPosition {
 		System.out.println("First setbit position for number: 18 is -> " + main.getFirstSetBitUsingLeftShift(18));
 		System.out.println("First setbit position for number: 5 is -> " + main.getFirstSetBitUsingLeftShift(5));
 		System.out.println("First setbit position for number: 32 is -> " + main.getFirstSetBitUsingLeftShift(32));
+
+		System.out.println("First setbit position for number: 18 is -> " + main.getFirstSetBitUsingRightShift(18));
+		System.out.println("First setbit position for number: 5 is -> " + main.getFirstSetBitUsingRightShift(5));
+		System.out.println("First setbit position for number: 32 is -> " + main.getFirstSetBitUsingRightShift(32));
 	}
 }
